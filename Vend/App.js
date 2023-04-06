9f4957d8d56c2376ae5b24013b016353e3b66030
JohnLewis9904 <90780139+JohnLewis9904@users.noreply.github.com>
2023-04-06 12:53:34 -0500

    discard tab navigation, added all to stack navigator

--- Vend/App.js
+++ Vend/App.js
@@ -1,33 +1,31 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './screens/HomeScreen';
 import SplashScreen from './screens/SplashScreen';
-import SignInScreen from './screens/SignInScreen';npm 
-import fake from './screens/fake';
+import SignInScreen from './screens/SignInScreen';
+import ShopScreen from './screens/ShopScreen';
+import ProfileScreen from './screens/ProfileScreen.js';
 
 const Stack = createNativeStackNavigator();
-// const Tab = createBottomTabNavigator();
 
 export default function App() {
  return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen options={{ headerShown: false}} name="HomeScreen" component={HomeScreen}
-        />
         <Stack.Screen options={{ headerShown: false}} name="SplashScreen" component={SplashScreen}
         />
         <Stack.Screen options={{ headerShown: false}} name="SignInScreen" component={SignInScreen}
         />
+        <Stack.Screen options={{ headerShown: false}} name="HomeScreen" component={HomeScreen}
+        />
+        <Stack.Screen options={{ headerShown: false}} name="ShopScreen" component={ShopScreen}
+        />
+        <Stack.Screen options={{ headerShown: false}} name="ProfileScreen" component={ProfileScreen}
+        />
       
       </Stack.Navigator>
-      {/* <Tab.Navigator>
-        <Tab.Screen name="HomeScreen" component={HomeScreen} />
-        <Tab.Screen name="ShopScreen" component={ShopScreen} />
-        <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
-      </Tab.Navigator> */}
     </NavigationContainer>
  );
 }
---
+++ Vend/screens/ProfileScreen.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  Touchable,
+  ScrollView,
+} from "react-native";
+// You can import from local files
+import colors from "../styles/colors";
+
+function ProfileScreen() {
+  return (
+    <View/>
+  );
+}
+export default ProfileScreen;
