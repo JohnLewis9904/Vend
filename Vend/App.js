51fc8a8d3a42daa5e42589a780db9b8558e641b0
JohnLewis9904 <90780139+JohnLewis9904@users.noreply.github.com>
2023-04-28 12:53:37 -0500

    adding to shop to get data, not showing but it is running

--- Vend/App.js
+++ Vend/App.js
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import HomeScreen from './screens/HomeScreen';
 import SplashScreen from './screens/SplashScreen';
 import SignInScreen from './screens/SignInScreen';
-import ShopScreen from './screens/ShopScreen';
+import ShopScreen from './screens/Shop';
 import ProfileScreen from './screens/ProfileScreen.js';
 import RegisterScren from './screens/RegisterScreen';
 import ImageScreenTemp from './screens/ImageUploadScreen';
@@ -16,6 +16,8 @@ export default function App() {
  return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen options={{ headerShown: false}} name="ShopScreen" component={ShopScreen}
+        />
         <Stack.Screen options={{ headerShown: false}} name="SplashScreen" component={SplashScreen}
         />
         <Stack.Screen options={{ headerShown: false}} name="SignInScreen" component={SignInScreen}
@@ -23,8 +25,6 @@ export default function App() {
         <Stack.Screen options={{ headerShown: false}} name="RegisterScreen" component={RegisterScren}
         />
         <Stack.Screen options={{ headerShown: false}} name="HomeScreen" component={HomeScreen}
-        />
-        <Stack.Screen options={{ headerShown: false}} name="ShopScreen" component={ShopScreen}
         />
         <Stack.Screen options={{ headerShown: false}} name="ProfileScreen" component={ProfileScreen}
         />
--- Vend/screens/Shop.js
+++ Vend/screens/Shop.js
@@ -7,41 +7,83 @@ import {
   Image,
   Touchable,
   ScrollView,
+  FlatList,
+  Button,
 } from "react-native";
 import { useState } from "react";
 
 import Item from "../components/Item";
-import SearchBar from "../components/Searchbar"
+// import SearchBar from "../components/Searchbar"
 // You can import from local files
 import colors from "../styles/colors";
+
+// Firebase imports
+import { auth, database, col } from "../firebase";
+import { collection, addDoc, getDocs } from "@firebase/firestore";
 
 function Shop() {
   const [username, setUsername] = useState('');
 
+  const [selected, setSelected] = React.useState('');
+
+const [items, setItems] = useState([]);
+React.useEffect(async () => {
+  col
+  .onSnapshot(
+    querySnapshot => {
+      const items = []
+      querySnapshot.forEach((doc) => {
+        const { Name, Price, Filename } = doc.data()
+        items.push({
+          id: doc.id,
+          Filename,
+          Name,
+          Price,
+        })
+      })
+      setItems(items)
+    }
+  )
+}, [])
+
   return (
     <SafeAreaView style={styles.container}>
+      <Button
+      title='Load Items'
+      style={styles.button}
+            onPress={() =>
+              getDocs(col)
+                .then((snapshot) => {
+                  let items = []
+                  snapshot.docs.forEach((doc) => {
+                  items.push({...doc.data(), id: doc.id})
+        })
+        console.log(items)
+    })
+    .catch(error =>{
+        console.log(erorr.message)
+    })
+          .then(console.log("item has been added"))
+        }
+          />
+      
         <View style={styles.header}>
-        <View style={styles.searchBar}>
+        {/* <View style={styles.searchBar}>
         <SearchBar fn="Username" input={setUsername} />
-        </View>
+        </View> */}
         <View style={styles.cartIcon}></View>
 
         </View>
         <View style={styles.itemList}>
-          <Item
-            header="Cotton T Shirt"
-            body="$32"
-            path={require("../assets/springReleaseBanner.jpeg")}
-          />
-           <Item
-            header="Nike Tech"
-            body="$65"
-            path={require("../assets/monkeyBanner.jpg")}
-          />
-           <Item
-            header="Adidas Puffer"
-            body="$20"
-            path={require("../assets/border.png")}
+          <FlatList style={{height:'100%'}}
+          data={items}
+          renderItem={({item}) => (
+            <Item
+              header={item.Name}
+              body={item.Price}
+              path={item.Filename}
+            />
+          )}
           />
         </View>
     </SafeAreaView>
@@ -54,6 +96,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     color: colors.slate,
+    padding: 50,
   },
   namePlate: {
     margin: "8%",
