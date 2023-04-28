4b3e79522d5ccafc8a5529d1c5e263170fc8f2dc
Kuldeep Debnath <63112459+KULDUDECS50@users.noreply.github.com>
2023-04-28 12:31:14 -0500

    Item from Snack

---
+++ Vend/components/Item.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Image,Text, StyleSheet } from 'react-native';
+import colors from '../styles/colors';
+import Constants from 'expo-constants';
+
+function Item({header, body, path}) {
+  return (
+    <View>
+      <View style={styles.viewContainer}>
+        <Image
+        style={styles.viewImage}
+          source={path}
+        />
+        <Text style={styles.viewHeader}>{header}</Text>
+        <Text style={styles.viewBody}>{body}</Text>
+        
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    height: 190,
+    width: 150,
+    borderRadius: 10,
+    backgroundColor: "black",
+    padding: 10,
+    margin:9,
+  },
+  viewImage: {
+
+    height: 100,
+    width: 130,
+    borderRadius: 5,
+    backgroundColor: colors.banana
+  },
+  viewHeader: {
+    fontSize: 14,
+    fontWeight: 600,
+    marginVertical: 9,
+    color: colors.slate,
+  },
+    viewBody: {
+    fontSize: 10,
+    fontWeight: 600,
+    marginBottom: 18,
+    color: colors.slate,
+  },
+});
+export default Item;
